a1dc53e87d78f1e3fd333b165eedef6ee3f364e1
EricLam404 <lameric9888@gmail.com>
2024-02-27 15:21:44 -0500

    Update edit and delete

--- app/profile/page.js
+++ app/profile/page.js
@@ -11,6 +11,8 @@ const MyProfile = () => {
 
     const [posts, setPosts] = useState([]);
 
+    const router = useRouter();
+
     useEffect(() => {
         const fetchPosts = async () => {
           const response = await fetch(`/api/users/${session?.user.id}/posts`);
@@ -22,12 +24,25 @@ const MyProfile = () => {
         if(session?.user.id) fetchPosts();
     }, [session?.user.id])
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
+        const hasConfirmed  = confirm("Are you sure you want to delete this prompt?");
 
+        if(hasConfirmed){
+            try {
+                await fetch(`/api/prompt/${post._id}`, {
+                    method: "DELETE"
+                })
+
+                const filteredPosts = posts.filter((p) => p._id !== post._id)
+                setPosts(filteredPosts);
+            } catch(error){
+                console.log(error)
+            }
+        }
     }
 
     return (
--- components/Form.js
+++ components/Form.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Form = ({type, post, setPost, submitting, handleSubmit}) => {
   return (
     <section className='w-full max-w-full flex-start flex-col'>
-      <h1 className='head__text text-left'>
+      <h1 className='head_text text-left'>
         <span className='blue_gradient'>{type} Post</span>
       </h1>
       <p className='desc text-left max-w-md'>
